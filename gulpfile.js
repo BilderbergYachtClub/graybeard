04463cdadb7bbc6fe4e52566d91edd43c882be88
Tim Warren <tim.warren@takl.com>
2020-03-06 11:19:29 -0600

    Added postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp')
 const pug = require('gulp-pug')
 const livereload = require('gulp-livereload')
+const postcss = require('gulp-postcss')
 
 gulp.task('html', () => {
   return gulp
@@ -10,7 +11,16 @@ gulp.task('html', () => {
     .pipe(livereload())
 })
 
+gulp.task('css', () => {
+  return gulp
+    .src(['src/styles/**/*.css', '!src/styles/**/_*.css'])
+    .pipe(postcss())
+    .pipe(gulp.dest('build/styles'))
+    .pipe(livereload())
+})
+
 gulp.task('dev', () => {
   livereload.listen()
   gulp.watch('src/**/*.pug', gulp.series('html'))
+  gulp.watch('src/**/*.css', gulp.series('css'))
 })
