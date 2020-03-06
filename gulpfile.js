7989d02df59308bb15316bedaad705cd6e92068a
Tim Warren <tim.warren@takl.com>
2020-03-06 10:44:36 -0600

    Added live reload

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,18 @@
 const { src, dest, watch } = require('gulp')
 const pug = require('gulp-pug')
+const livereload = require('gulp-livereload')
 
 function html () {
   return src(['src/**/*.pug', '!src/includes/*'])
     .pipe(pug())
     .pipe(dest('build/'))
+    .pipe(livereload())
 }
 
 // exports.js = js;
 // exports.css = css;
 exports.html = html
 exports.watch = function () {
+  livereload.listen()
   watch('src/**/*.pug', html)
 }
