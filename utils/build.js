ea536dfde79206c6a635f7479ddb3fba0db10fd9
Tim Warren <kreas@pm.me>
2020-04-02 12:18:28 -0500

    moved templates to utils/build

---
+++ utils/build.js
@@ -0,0 +1,21 @@
+const pug = require('gulp-pug')
+const markdown = require('gulp-markdown')
+
+const handleError = error => {
+  if (process.env.NODE_ENV === 'produciton') {
+    throw new Error(error)
+  } else {
+    console.error(error)
+  }
+}
+
+module.exports = {
+  templates: {
+    pug() {
+      return pug({ pretty: true }).on('error', handleError)
+    },
+    markdown() {
+      return markdown()
+    }
+  }
+}
